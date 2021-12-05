78aad046bcce580cb992c39f595a13e65afee5c2
Hakan <momo@stupan.se>
2021-12-05 16:55:35 +0100

    Templates: Updating cmake

--- cmake/PluginLibs.cmake
+++ cmake/PluginLibs.cmake
@@ -1,19 +1,38 @@
+# ~~~
+# Summary:      Find and link general plugin libraries
+# License:      GPLv3+
+# Copyright (c) 2021 Alec Leamas
 #
 # Find and link general libraries to use: gettext, wxWidgets and OpenGL
-#
+# ~~~
+
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
 
 find_package(Gettext REQUIRED)
 
-set(wxWidgets_USE_DEBUG OFF)
-set(wxWidgets_USE_UNICODE ON)
-set(wxWidgets_USE_UNIVERSAL OFF)
-set(wxWidgets_USE_STATIC OFF)
+#
+# Windows environment check
+#
+set(_bad_win_env_msg [=[
+%WXWIN% is not present in environment, win_deps.bat has not been run.
+Build might work, but most likely fail when not finding wxWidgets.
+Run buildwin\win_deps.bat or set %WXWIN% to mute this message.
+]=])
 
-# Prefer libGL.so to libOpenGL.so, see CMP0072
-set(OpenGL_GL_PREFERENCE "LEGACY")
+if (WIN32 AND NOT DEFINED ENV{WXWIN})
+  message(WARNING ${_bad_win_env_msg})
+endif ()
 
+#
+# OpenGL
+#
 find_package(OpenGL)
-if (TARGET OpenGL::GL)
+if (TARGET OpenGL::OpenGL)
+  target_link_libraries(${PACKAGE_NAME} OpenGL::OpenGL)
+elseif (TARGET OpenGL::GL)
   target_link_libraries(${PACKAGE_NAME} OpenGL::GL)
 else ()
   message(WARNING "Cannot locate usable OpenGL libs and headers.")
@@ -31,10 +50,31 @@ if (APPLE)
     message(WARNING "Cannot locate OpenGL header file gl.h")
   endif ()
 endif ()
+if (WIN32)
+  if (EXISTS "${PROJECT_SOURCE_DIR}/libs/WindowsHeaders")
+    add_subdirectory("${PROJECT_SOURCE_DIR}/libs/WindowsHeaders")
+    target_link_libraries(${PACKAGE_NAME} windows::headers)
+  else ()
+    message(STATUS
+      "WARNING: WindowsHeaders library is missing, OpenGL unavailable"
+    )
+  endif ()
+endif ()
 
-set(wxWidgets_USE_LIBS base core net xml html adv stc)
+#
+# wxWidgets
+#
+set(wxWidgets_USE_DEBUG OFF)
+set(wxWidgets_USE_UNICODE ON)
+set(wxWidgets_USE_UNIVERSAL OFF)
+set(wxWidgets_USE_STATIC OFF)
 
-find_package(wxWidgets REQUIRED base core net xml html adv)
+set(WX_COMPONENTS base core net xml html adv stc aui)
+if (TARGET OpenGL::OpenGL OR TARGET OpenGL::GL)
+  list(APPEND WX_COMPONENTS gl)
+endif ()
+
+find_package(wxWidgets REQUIRED ${WX_COMPONENTS})
 if (MSYS)
   # This is just a hack. I think the bug is in FindwxWidgets.cmake
   string(
@@ -42,4 +82,5 @@ if (MSYS)
     wxWidgets_INCLUDE_DIRS ${wxWidgets_INCLUDE_DIRS}
   )
 endif ()
-include(${wxWidgets_USE_FILE})	
+include(${wxWidgets_USE_FILE})
+target_link_libraries(${PACKAGE_NAME} ${wxWidgets_LIBRARIES})
